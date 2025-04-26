66deb1374c3ef3df596d30d089ce96f94d8f3b4e
Ayesha Taranum <153802441+Ayesha-Taranum@users.noreply.github.com>
2025-04-26 14:31:26 +0530

    Server Files Commited

---
+++ server/src/main/java/com/recipeapp/recipeapp/springbootrecipeapp/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.recipeapp.recipeapp.springbootrecipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
